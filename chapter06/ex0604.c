935276f5206d641fe9cf6dba1b85d211faa9ea5e
Meowbutu <mewmewdevart@gmail.com>
2022-11-30 16:19:11 -0300

    to be continue

--- chapter06/ex0604.c
+++ chapter06/ex0604.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+#define DIM 3
+#define ESPACO ' '
+
+void inic(char s[][DIM]);
+void mostra(char s[DIM][DIM]);
+int Ganhou(char g[DIM][DIM], char ch);
+int Linha(char v[], char c);
+int Coluna(char g[DIM][DIM],  int col, char ch);
+
 int main (void)
 {
 
