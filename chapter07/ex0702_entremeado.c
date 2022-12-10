17f5e6d606c39c23c61b1a272b7460c7a1f1b7a8
Meowbutu <mewmewdevart@gmail.com>
2022-12-10 00:47:37 -0300

    more exercise

--- chapter07/ex0702_entremeado.c
+++ chapter07/ex0702_entremeado.c
@@ -6,7 +6,7 @@ int ft_strlen(char *str);
 int main (void)
 {
         char str[30] = "ABCDEFGHIJKLMN";
-        int number = 1;
+        int number = 3;
 
         printf("%s", ft_entremeado(str, number));
         return (0);
@@ -22,10 +22,12 @@ char *ft_entremeado(char *str, int n)
                 return (str);
 
         j = i = 0;
-        while(str[i] != '\0')
+        while(i < len) //i menor que len? 
         {
+                str[j++] = str[i]; // 2) o resultado é somado e aplicado no str[i] que em seguida envia as novas posições para o str[j++] que anda uma posição logo em seguida
+                i += n+1; // 1) i = i + n + 1; 
         }
-        str[i] = '\0';
+        str[j] = '\0';
         return (str);
 }
 
